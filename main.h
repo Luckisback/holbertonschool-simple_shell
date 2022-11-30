c2856400df36870171c498529b43ef479f57b753
bapt2 <5711@holbertonstudents.com>
2022-11-30 10:48:34 -0800

    added readme and main.h files for simple shell

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL SIMPLE_SHELL
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+
+
+
+
+
+#endif SIMPLE_SHELL

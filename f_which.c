1a267b01fc30e08ae0df24fcdc7ed6fe77f6af3f
jlucbilo@gmail.com <jlucbilo@gmail.com>
2022-12-09 02:13:28 -0800

    mofication of var type, and the call of the next function

--- f_which.c
+++ f_which.c
@@ -7,42 +7,55 @@
  * enter through sdtin
  * Return: 0 for success
  **/
-int _which(char **tok_tab)
+char *_which(char **tok_tab)
 {
-	char *cmd = tok_tab[0];
-	/*int notfound = 0;*/
+	int notfound = 0;
 	char *_token = NULL, *_path[] = {NULL};
 	char *src_path = getenv("PATH");
-	/*struct stat st;*/
+	struct stat st;
 
 	if (!src_path)
 	{
 		perror("there is an allocation pb");
 		exit(EXIT_FAILURE);
 	}
-	_token = malloc(sizeof(char) * strlen(src_path));
-	_token = src_path;
-	printf("PATH TO VALEUR DE TOKEN:  %s\n", _token);
+	_token = malloc(sizeof(char*) * strlen(src_path));
+	strcpy(_token, src_path);
 
 	if (_token != NULL)
 		_token = strtok(src_path, ":");
 
-	printf("ICI LE PREMIER TOKEN: %s\n", _token);
-
-	if (_token)
+	while (_token)
 	{
 		_path[0] = malloc(sizeof(_token) * strlen(src_path));
-		_path[0] = _token;
+		if (!_path[0])
+		{
+			free(_path[0]);
+			exit(EXIT_FAILURE);
+		}
+		_path[0] = strcpy(_path[0], _token);
 		strcat(_path[0], "/");
-		strcat(_path[0], cmd);
-		printf("ICI LA CONCATENATION:  %s\n", _path[0]);
+		strcat(_path[0], tok_tab[0]);
 
+		if (stat(_path[0], &st) == 0)
+		{
+			printf("%s\n", _path[0]);
+			notfound = 1;
+			return (_path[0]);
+		}
+
+		_token = strtok(NULL, ":");
 		if (!_token)
 		{
 			perror("there is an allocation pb");
-			exit(EXIT_FAILURE);
+			free(_token);
+                        exit(EXIT_FAILURE);
 		}
-		f_execut(_path[0]);/*call of the function to execut the cmd*/
 	}
-	return (0);
+	if (notfound == 0)
+		printf("NOT FOUND\n");
+	
+	f_execut(_path);
+
+        return (0);
 }
